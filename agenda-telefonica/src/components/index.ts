a5163cdd8b37b185536f43e6d75395f62ce44660
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2021-04-10 10:38:43 -0300

    chore: connect dropdown with main page

--- agenda-telefonica/src/components/index.ts
+++ agenda-telefonica/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Input } from './Input';
 export { default as Modal } from './Modal';
 export { default as Tooltip } from './Tooltip';
 export { default as Search } from './Search';
+export { default as DropdownButton } from './DropdownButton';
