b4d3b97db49bb0681410e676c7a2defcd3ad79b9
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2021-04-11 11:01:26 -0300

    chore: recover and delete contacts from localStorage

--- agenda-telefonica/src/App.tsx
+++ agenda-telefonica/src/App.tsx
@@ -1,14 +1,11 @@
 import React, { useState, useRef, useCallback, useEffect } from 'react';
-import { Container, Header, Button, StyledForm,CardsContainer, CTitle } from './styles';
+import { Container, Header, Button, StyledForm, CardsContainer, CTitle } from './styles';
 import { Search, Modal, Input, InputMask, DropdownButton, UserCard } from '~/components';
 import './App.css';
 import { FormHandles } from '@unform/core';
-import { toast, ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import * as Yup from 'yup';
-import { IoMdPersonAdd } from 'react-icons/io';
+import { IoMdPersonAdd, IoMdTrash, IoIosArrowRoundBack } from 'react-icons/io';
 import getValidationErrors from '~/validators/getValidationsErrors';
-import { mockContacts } from '~/utils/mockContacts';
 import mergeSort from '~/utils/mergeSort';
 import IContact from '~/interfaces/IContact';
 
@@ -16,103 +13,131 @@ const App: React.FC = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const [searchName, setSearchName] = useState(true);
   const [searchNumber, setSearchNumber] = useState(false);
-  const [contacts, setContacts] = useState<IContact[]>(mockContacts);
+  const [contacts, setContacts] = useState<IContact[]>([]);
   const [searchResult, setSearchResult] = useState<IContact>();
   const formRef = useRef<FormHandles>(null);
 
   const handleSubmit = useCallback(
-    async (data) => {
+    async (data: IContact) => {
       try {
         const schema = Yup.object().shape({
-          nome: Yup.string().required('Nome de contato obrigatório.'),
-          telefone: Yup.string().required('Telefone obrigatório.'),
+          name: Yup.string().required('Nome de contato obrigatório.'),
+          phone: Yup.string().required('Telefone obrigatório.'),
         });
 
         await schema.validate(data, {
           abortEarly: false,
         });
+        console.log(contacts);
+        const contactsArray = contacts;
+        const contact: IContact = {
+          name: data.name,
+          phone: data.phone,
+        };
+        contactsArray.push(contact);
+        console.log(contactsArray);
+        const newContacts = mergeSort(contactsArray, searchName ? 'name' : 'phone');
+        setContacts(newContacts);
+        localStorage.setItem('contactsArray', JSON.stringify(newContacts));
 
-        // console.log(data);
+        console.log(data);
       } catch (error) {
         if (error instanceof Yup.ValidationError) {
           // console.log(error);
           const errors = getValidationErrors(error);
 
-          Object.values(errors).forEach((value: string) => {
-            toast.error(`${value}`);
-          });
-
           formRef.current?.setErrors(errors);
-          console.log(errors);
-        }
-        if (error?.response?.status === 400) {
-          toast.success('modelo de envio');
+          // console.log(errors);
         }
       }
     },
     [],
   );
 
-  useEffect(()=>{
-    const newContacts = mergeSort(contacts, searchName ? 'name' : 'phone')
+  useEffect(() => {
+    const contactsArray: IContact[] = JSON.parse(localStorage.getItem('contactsArray') || '[]');
+    const newContacts = mergeSort(contactsArray, searchName ? 'name' : 'phone')
     setContacts(newContacts);
   }, [searchNumber, searchName]);
 
+  const deleteContacts = () => {
+    localStorage.removeItem('contactsArray');
+    setContacts([]);
+  };
+
+
   return (
-    <>
-      <ToastContainer />
-      <Container>
-        <Header>
-          <Search
-            searchResult={searchResult}
-            setSearchResult={setSearchResult}
-            searchName={searchName}
-            contacts={contacts} 
-          />
-          <DropdownButton 
-            searchName={searchName} 
-            setSearchName={setSearchName}
-            searchNumber={searchNumber} 
-            setSearchNumber={setSearchNumber} 
-          />
-          <Button onClick={()=>{setModalVisible(true)}}>
-            <IoMdPersonAdd size={30} />
-          </Button>
-        </Header>
-        <CardsContainer>
-          {!searchResult &&
+    <Container>
+      <Header>
+        <Search
+          searchResult={searchResult}
+          setSearchResult={setSearchResult}
+          searchName={searchName}
+          contacts={contacts}
+        />
+        <DropdownButton
+          searchName={searchName}
+          setSearchName={setSearchName}
+          searchNumber={searchNumber}
+          setSearchNumber={setSearchNumber}
+        />
+        <Button onClick={() => { setModalVisible(true) }}>
+          <IoMdPersonAdd size={30} />
+        </Button>
+      </Header>
+      <CardsContainer>
+        {!searchResult &&
           <>
-            <CTitle>Seus contatos</CTitle>
-              {contacts?.map((contact)=>
-                <UserCard key={contact.phone} contact={contact}/>
-              )}
+            <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: '100%',
+            }}>
+              <CTitle style={{ marginLeft: 'auto' }}>Seus contatos</CTitle>
+              <Button style={{ marginLeft: 'auto' }} onClick={deleteContacts}>
+                <IoMdTrash size={30} />
+              </Button>
+            </div>
+            {contacts?.map((contact) =>
+              <UserCard key={contact.phone} contact={contact} />
+            )}
           </>
-          }
-          {!!searchResult &&
+        }
+        {!!searchResult &&
           <>
-            <CTitle>Sua busca</CTitle>
-            <UserCard key={searchResult.phone} contact={searchResult}/>
+            <div style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: '100%',
+            }}>
+              <CTitle style={{ marginLeft: 'auto' }}>Sua busca</CTitle>
+              <Button style={{ marginLeft: 'auto' }} onClick={() => { setSearchResult(undefined) }}>
+                <IoIosArrowRoundBack size={30} />
+              </Button>
+            </div>
+            <UserCard key={searchResult.phone} contact={searchResult} />
           </>
-          }
-        </CardsContainer>
-        <Modal
-          modalVisible={modalVisible}
-          setModalVisible={setModalVisible}
-          btnClose={true}
-        >
-          <StyledForm onSubmit={handleSubmit} ref={formRef}>
-            <h1>Cadastrar contato</h1>
-            <Input name="nome" placeholder="Nome Completo" />
-            <InputMask
-              name="telefone"
-              mask="(99) 9 9999-9999"
-              placeholder="Telefone"
-            />
-            <button onClick={() => formRef.current?.submitForm()}>Cadastrar</button>
-          </StyledForm>
-        </Modal>
-      </Container>
-    </>
+        }
+      </CardsContainer>
+      <Modal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        btnClose={true}
+      >
+        <StyledForm onSubmit={handleSubmit} ref={formRef}>
+          <h1>Cadastrar contato</h1>
+          <Input name="name" placeholder="Nome Completo" />
+          <InputMask
+            name="phone"
+            mask="(99) 9 9999-9999"
+            placeholder="Telefone"
+          />
+          <button onClick={() => formRef.current?.submitForm()}>Cadastrar</button>
+        </StyledForm>
+      </Modal>
+    </Container>
   );
 }
 
--- agenda-telefonica/src/utils/mockContacts.ts
+++
@@ -1,5 +0,0 @@
-export const mockContacts = [
-  { name: 'Murilindo Gomes', phone: '77 7777 7777'},
-  { name: 'Gabriel Paiva', phone: '88 8888 8888'},
-  { name: 'Elision KaDeus', phone: '99 9999 9999'}
-];
